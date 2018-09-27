3fdc5de58ed123a17ebfb920ca6e88b0ecd7efbf
Pablo2017 <pablonavarro2015114121@gmail.com>
2018-09-27 15:54:44 -0600

    separación de archivos Server - Functions

---
+++ nodeServer/functions.js
@@ -0,0 +1,301 @@
+//Funcion para obtener el File System
+function getDirectories(req, res, data) {
+    var folder = data.carpetaActual;
+    if (validarRuta(folder)) {
+        var fileSystem = getFileSystem(folder);
+        fileSystem.folders.unshift('./filesUploaded');
+        return sendBack(res, 'OK', 'File System', fileSystem);
+    } else {
+        return sendBack(res, 'ERROR', 'Acceso a Carpeta Denegado');
+    }
+}
+//Funcion para obtnener el contenido plano de un archivo
+function getPlainText(req, res, data) {
+    var rutaArchivo = data.rutaArchivo;
+    log("/getPlainText " + rutaArchivo);
+    if (validarRuta(rutaArchivo)) {
+        return returnPlainText(req, res, rutaArchivo);
+    } else {
+        return sendBack(res, 'ERROR', 'Acceso a Archivo Denegado');
+    }
+}
+//Funcion para borrar un archivo
+function deleteFile(req, res, data) {
+    var rutaArchivo = data.rutaArchivo;
+    log("/deleteFile " + rutaArchivo);
+    if (validarRuta(rutaArchivo)) {
+        return deleteFile(req, res, rutaArchivo);
+    } else {
+        return sendBack(res, 'ERROR', 'Acceso a Archivo Denegado');
+    }
+}
+//Funcion para actulizar el contenido de un archivo plano
+function writeOnFile(req, res, data) {
+    var rutaArchivo = data.rutaArchivo;
+    var nuevoContenido = data.nuevoContenido;
+    log("/writeFile " + rutaArchivo);
+    if (validarRuta(rutaArchivo)) {
+        return writeFile(req, res, rutaArchivo, nuevoContenido);
+    } else {
+        return sendBack(res, 'ERROR', 'Acceso a Archivo Denegado');
+    }
+}
+//
+function mkDir(req, res, data) {
+    var rutaCarpeta = data.rutaCarpeta;
+    log('/mkDir ' + rutaCarpeta);
+    if (validarRuta(rutaCarpeta)) {
+        var s = createDirectory(rutaCarpeta, res);
+        if (s == 'OK') {
+            return sendBack(res, 'OK', 'Folder Created');
+        } else {
+            return sendBack(res, 'ERROR', s);
+        }
+    } else {
+        return sendBack(res, 'ERROR', 'Acceso a Carpeta Denegado');
+    }
+}
+
+function renameFile(req, res, data) {
+    var rutaArchivo = data.rutaArchivo;
+    var nuevoNombre = data.nuevoNombre;
+    var ruta = data.ruta;
+    log('/renameFile ' + rutaArchivo);
+    if (validarRuta(rutaArchivo) && validarRuta(ruta)) {
+        return renombrarArchivo('./' + rutaArchivo, './' + ruta, nuevoNombre, res)
+    } else {
+        return sendBack(res, 'ERROR', 'Acceso a Carpeta Denegado');
+    }
+}
+
+function mkTextFile(req, res, data) {
+    var rutaArchivo = data.rutaArchivo;
+    var contenido = data.contenido;
+    log("/mkTextFile " + rutaArchivo);
+    if (validarRuta(rutaArchivo)) {
+        return writeFile(req, res, rutaArchivo, contenido);
+    } else {
+        return sendBack(res, 'ERROR', 'Acceso a Archivo Denegado');
+    }
+}
+
+function uploadFile(req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        if (files['file'] && validarRuta(fields.ruta)) {
+            var oldpath = files.file.path;
+            var newpath = fields.ruta + '/' + files.file.name; //'./filesUploaded/' + files.file.name;
+            var extension = getExtension(files.file.name);
+            if (extensiones.indexOf(extension) >= 0) {
+                fs.rename(oldpath, newpath, function (err) {
+                    if (err) throw err;
+                    return sendBack(res, 'OK', 'Archivo Subido');
+                });
+            } else {
+                return sendBack(res, 'ERROR', 'Extensión de archivo no permitida: ' + extension);
+            }
+        } else {
+            return sendBack(res, 'ERROR', 'Archivo no se cargó al servidor');
+        }
+    });
+}
+
+function returnFile(url, res) {
+    fs.readFile(url, function (err, content) {
+        if (err) {
+            res.writeHead(400, {
+                'Content-type': 'text/html'
+            })
+            console.log(err);
+            return sendBack(res, 'ERROR', 'Error al retornar el archivo');
+        } else {
+            //specify Content will be an attachment
+            res.setHeader('Content-disposition', 'attachment; filename=' + getFileNameFromURL(url).replace(/,/g, "_"));
+            res.setHeader('FileName', getFileNameFromURL(url));
+            return res.end(content);
+        }
+    });
+}
+
+function validarRuta(r) {
+    if (r.substr(0, 15) === './filesUploaded') {
+        return true;
+    }
+    return false;
+}
+
+function returnPlainText(req, res, rutaArchivo) {
+    fs.readFile(rutaArchivo, function (err, text) {
+        if (err) {
+            return notFound(req, res);
+        } else {
+            res.setHeader("Content-Type", "text/plain");
+            return res.end(text);
+        }
+    });
+}
+
+
+function createDirectory(path) {
+    fs.mkdir(path, (err) => {
+        if (err) {
+            log(err);
+            return err.message;
+        } else {
+            return 'OK'
+        }
+    });
+}
+
+//Lee un archivo y devuelve su contenido
+function getFile(req, res, header) {
+    if (req.url == "/") {
+        url = "./index.html";
+    } else {
+        url = req.url;
+        url = limpiarURL(url);
+        url = "./" + url;
+    }
+    fs.readFile(url, function (err, text) {
+        if (err) {
+            return notFound(req, res);
+        } else {
+            res.setHeader("Content-Type", header);
+            return res.end(text);
+        }
+    });
+}
+
+// Sirve para quitar lo que haya después de que aparece un '?' en la url (solicitudes POST)
+function limpiarURL(url) {
+    var indexPreg = url.indexOf("?");
+    url = url.indexOf("?") != -1 ? url.substring(0, indexPreg) : url;
+    return url
+}
+
+// envía al cliente un un objeto en formato string
+function sendBack(res, estado, mensaje, data) {
+    var obj = {
+        estado: estado,
+        mensaje: mensaje,
+        data: data
+    }
+    return res.end(JSON.stringify(obj));
+}
+
+//Procesa los parametros de la solicitud POST
+function processRequestData(request, callback, procesarDatosPost) {
+    if (procesarDatosPost == true) {
+        let data = [];
+        request.on('data', (chunk) => {
+            data.push(chunk);
+        }).on('end', () => {
+            data = JSON.parse(Buffer.concat(data).toString());
+            return callback(data);
+        });
+    } else {
+        return callback();
+    }
+}
+
+//Obtener extension de un nombre de archivo
+function getExtension(filename) {
+    var ext = path.extname(filename || '').split('.');
+    return ext[ext.length - 1];
+}
+
+//Obtiene todos los archivos de un directorio
+function getFileSystem(dir, files_) {
+    files_ = files_ || {
+        files: [],
+        folders: []
+    };
+    var files = fs.readdirSync(dir);
+    for (var i in files) {
+        var name = dir + '/' + files[i];
+        elem = fs.statSync(name);
+        if (elem.isDirectory()) {
+            files_.folders.push(name);
+            getFileSystem(name, files_);
+        } else {
+            var fileSize = elem.size;
+            fileSize /= 1048576;
+            files_.files.push({
+                name: name,
+                size: formatearFloat(fileSize)
+            });
+        }
+    }
+    return files_;
+}
+
+function getFileNameFromURL(fileURL) {
+    return fileURL.substr(fileURL.lastIndexOf('/') + 1)
+}
+
+function deleteFile(req, res, rutaArchivo) {
+    fs.unlink(rutaArchivo, (err) => {
+        if (err) {
+            log("FILE NOT DELETED: " + rutaArchivo);
+            return res.end("No se puede borrar el archivo");
+        };
+        log("FILE DELETED: " + rutaArchivo);
+        return res.end("OK");
+    });
+}
+
+function renombrarArchivo(rutaArchivo, ruta, newName, res) {
+    fs.rename(rutaArchivo, ruta + newName, (err) => {
+        if (err) {
+            log(err)
+            log("FILE NOT UPDATED: " + rutaArchivo);
+            return res.end("ERROR");
+        };
+        log("FILE UPDATED: " + rutaArchivo);
+        return res.end("OK");
+    });
+}
+
+function writeFile(req, res, rutaArchivo, nuevoContenido) {
+    fs.writeFile(rutaArchivo, nuevoContenido, (err) => {
+        if (err) {
+            log("FILE NOT WRITTED: " + rutaArchivo);
+            return res.end("No se puede escribir el archivo");
+        };
+        log("FILE WRITTED: " + rutaArchivo);
+        return res.end("OK");
+    });
+}
+
+function notFound(req, res) {
+    log("NOT FOUND:  " + req.url);
+    fs.readFile('./inc/404-Not-Found.html', function (err, text) {
+        if (err) {
+            log(text);
+            log("ERROR:  " + err);
+            return res.end("ERROR:  " + req.url);
+        } else {
+            res.setHeader("Content-Type", "text/html");
+            return res.end(text);
+        }
+    });
+}
+
+function isBlog(hostName) {
+    if (hostName.includes('blog.')) {
+        log('Its the blog')
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function formatearFloat(n, d) {
+    // parsea el string n a float y lo limita a 2 decimales
+    return parseFloat(parseFloat(n).toFixed(d ? d : 2));
+}
+
+//Imprime objeto
+function log(o) {
+    console.log(o);
+}
--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,5 +1,6 @@
 var http = require("http"); //Crea servidor
 var fs = require('fs'); //Acceder al file system
+eval(fs.readFileSync('functions.js')+'');
 var path = require('path'); //Manejar rutas del FS
 var formidable = require('formidable'); //Cargar archivos al servidor
 var port = 8081; // 80;
@@ -8,182 +9,55 @@ const extensiones = ['exe', 'mp4', 'avi', 'mkv', 'mp3', 'png', 'ico', 'jpg', 'jp
 
 //Crea el servidor y procesa las solicitudes de archivos o apis
 var server = http.createServer(function (req, res) {
-    //    t ry {
-    req.url = decodeURIComponent(req.url);
-    if (req.url.substring(0, 4) == "/api") {
-        procesarApi(req, res);
-    } else {
-        procesarArchivo(req, res);
-    }
-    /*} catch (err) {
+    try {
+        req.url = decodeURIComponent(req.url);
+        if (req.url.substring(0, 4) == "/api") {
+            procesarApi(req, res);
+        } else {
+            procesarArchivo(req, res);
+        }
+    } catch (err) {
         return res.end("Ha ocurrido un error: " + err);
-    }*/
+    }
 });
-
 // START SERVER
 console.log("Starting web server at " + serverUrl + ":" + port);
 server.listen(port);
-
 
 //Ejecuta el api solicitada
 function procesarApi(req, res) {
     var api = req.url.substring(4);
     log("Procesando API : " + api);
-    if (api != '/uploadFile') {
-        if (req.method === 'POST') {
-            processRequestData(req, (data) => { //En data están todos los parámetros del post
-                switch (api) {
-                    case "/getDirectories":
-                        var folder = data.carpetaActual;
-                        if (folder.substr(0, 15) === './filesUploaded') {
-                            var fileSystem = getFileSystem(folder);
-                            fileSystem.folders.unshift('./filesUploaded');
-                            return sendBack(res, 'OK', 'File System', fileSystem);
-                        } else {
-                            return sendBack(res, 'ERROR', 'Acceso a Carpeta Denegado');
-                        }
-                        break;
-                    case "/getFile":
-                        var rutaArchivo = data.rutaArchivo;
-                        log("/getFile " + rutaArchivo);
-                        if (rutaArchivo.substr(0, 15) === './filesUploaded') {
-                            return returnFile(rutaArchivo, res);
-                        } else {
-                            return sendBack(res, 'ERROR', 'Acceso a Archivo Denegado');
-                        }
-                        break;
-                    case "/getPlainText":
-                        var rutaArchivo = data.rutaArchivo;
-                        log("/getFile " + rutaArchivo);
-                        if (rutaArchivo.substr(0, 15) === './filesUploaded') {
-                            return returnPlainText(req, res, rutaArchivo);
-                        } else {
-                            return sendBack(res, 'ERROR', 'Acceso a Archivo Denegado');
-                        }
-                        break;
-                    case "/deleteFile":
-                        var rutaArchivo = data.rutaArchivo;
-                        log("/deleteFile " + rutaArchivo);
-                        if (rutaArchivo.substr(0, 15) === './filesUploaded') {
-                            return deleteFile(req, res, rutaArchivo);
-                        } else {
-                            return sendBack(res, 'ERROR', 'Acceso a Archivo Denegado');
-                        }
-                        break;
-                    case "/writeFile":
-                        var rutaArchivo = data.rutaArchivo;
-                        var nuevoContenido = data.nuevoContenido;
-                        log("/writeFile " + rutaArchivo);
-                        if (rutaArchivo.substr(0, 15) === './filesUploaded') {
-                            return writeFile(req, res, rutaArchivo, nuevoContenido);
-                        } else {
-                            return sendBack(res, 'ERROR', 'Acceso a Archivo Denegado');
-                        }
-                        break;
-                    case "/mkDir":
-                        var rutaCarpeta = data.rutaCarpeta;
-                        log('/mkDir ' + rutaCarpeta);
-                        if (rutaCarpeta.substr(0, 15) === './filesUploaded') {
-                            var s = mkDir(rutaCarpeta, res);
-                            if (s == 'OK') {
-                                return sendBack(res, 'OK', 'Folder Created');
-                            } else {
-                                return sendBack(res, 'ERROR', s);
-                            }
-                        } else {
-                            return sendBack(res, 'ERROR', 'Acceso a Carpeta Denegado');
-                        }
-                        break;
-                    case "/renameFile":
-                        var rutaArchivo = data.rutaArchivo;
-                        var nuevoNombre = data.nuevoNombre;
-                        var ruta = data.ruta;
-                        log('/renameFile ' + rutaArchivo);
-                        if (rutaArchivo.substr(0, 15) === './filesUploaded' && ruta.substr(0, 15) === './filesUploaded') {
-                            return renameFile('./'+rutaArchivo, './'+ruta, nuevoNombre,res)
-                        } else {
-                            return sendBack(res, 'ERROR', 'Acceso a Carpeta Denegado');
-                        }
-                        break;
-                    case "/mkTextFile":
-                        var rutaArchivo = data.rutaArchivo;
-                        var contenido = data.contenido;
-                        log("/mkTextFile " + rutaArchivo);
-                        if (rutaArchivo.substr(0, 15) === './filesUploaded') {
-                            return writeFile(req, res, rutaArchivo, contenido);
-                        } else {
-                            return sendBack(res, 'ERROR', 'Acceso a Archivo Denegado');
-                        }
-                        break;
-                    default:
-                        return res.end("ERROR API POST: " + JSON.stringify(data));
-                }
-            });
-        } else if (req.method === 'GET') {
-            return res.end("API GET PROCESADA");
-        }
-    } else { //To Upload a File
-        var form = new formidable.IncomingForm();
-        form.parse(req, function (err, fields, files) {
-            log(fields.ruta)
-            if (files['file'] && fields.ruta.substr(0, 15) === './filesUploaded') {
-                var oldpath = files.file.path;
-                var newpath = fields.ruta + '/' + files.file.name; //'./filesUploaded/' + files.file.name;
-                var extension = getExtension(files.file.name);
-                if (extensiones.indexOf(extension) >= 0) {
-                    fs.rename(oldpath, newpath, function (err) {
-                        if (err) throw err;
-                        return sendBack(res, 'OK', 'Archivo Subido');
-                    });
-                } else {
-                    return sendBack(res, 'ERROR', 'Extensión de archivo no permitida: ' + extension);
-                }
-            } else {
-                return sendBack(res, 'ERROR', 'Archivo no se cargó al servidor');
+    if (req.method === 'POST') {
+        var procesarDatosPost = (api !== '/uploadFile' ? true : false);
+        processRequestData(req, (data) => { //En data están todos los parámetros del post
+            switch (api) {
+                case "/getDirectories":
+                    return getDirectories(req, res, data);
+                case "/getFile":
+                    return getFile(req, res, data);
+                case "/getPlainText":
+                    return getPlainText(req, res, data);
+                case "/deleteFile":
+                    return deleteFile(req, res, data);
+                case "/writeFile":
+                    return writeOnFile(req, res, data);
+                case "/mkDir":
+                    return mkDir(req, res, data);
+                case "/renameFile":
+                    return renameFile(req, res, data);
+                case "/mkTextFile":
+                    return mkTextFile(req, res, data);
+                case "/uploadFile":
+                    return uploadFile(req, res);
+                default:
+                    return res.end("ERROR API POST: " + JSON.stringify(data));
             }
-        });
+        }, procesarDatosPost);
+    } else if (req.method === 'GET') {
+        return res.end("API GET PROCESADA");
     }
 }
-
-function returnPlainText(req, res, rutaArchivo) {
-    fs.readFile(rutaArchivo, function (err, text) {
-        if (err) {
-            return notFound(req, res);
-        } else {
-            res.setHeader("Content-Type", "text/plain");
-            return res.end(text);
-        }
-    });
-}
-
-function returnFile(url, res) {
-    fs.readFile(url, function (err, content) {
-        if (err) {
-            res.writeHead(400, {
-                'Content-type': 'text/html'
-            })
-            console.log(err);
-            return sendBack(res, 'ERROR', 'Error al retornar el archivo');
-        } else {
-            //specify Content will be an attachment
-            res.setHeader('Content-disposition', 'attachment; filename=' + getFileNameFromURL(url).replace(/,/g, "_"));
-            res.setHeader('FileName', getFileNameFromURL(url));
-            return res.end(content);
-        }
-    });
-}
-
-function mkDir(path) {
-    fs.mkdir(path, (err) => {
-        if (err) {
-            log(err);
-            return err.message;
-        } else {
-            return 'OK'
-        }
-    });
-}
-
 //Segun el tipo de archivo solicitado configura el header correspondiente y retorna el contenido del archivo
 function procesarArchivo(req, res) {
     log("Procesando ARCHIVO : " + req.url);
@@ -204,153 +78,4 @@ function procesarArchivo(req, res) {
     } else {
         return notFound(req, res);
     }
-}
-
-//Lee un archivo y devuelve su contenido
-function getFile(req, res, header) {
-    if (req.url == "/") {
-        url = "./index.html";
-    } else {
-        url = req.url;
-        url = limpiarURL(url);
-        url = "./" + url;
-    }
-    fs.readFile(url, function (err, text) {
-        if (err) {
-            return notFound(req, res);
-        } else {
-            res.setHeader("Content-Type", header);
-            return res.end(text);
-        }
-    });
-}
-
-// Sirve para quitar lo que haya después de que aparece un '?' en la url (solicitudes POST)
-function limpiarURL(url) {
-    var indexPreg = url.indexOf("?");
-    url = url.indexOf("?") != -1 ? url.substring(0, indexPreg) : url;
-    return url
-}
-
-// envía al cliente un un objeto en formato string
-function sendBack(res, estado, mensaje, data) {
-    var obj = {
-        estado: estado,
-        mensaje: mensaje,
-        data: data
-    }
-    return res.end(JSON.stringify(obj));
-}
-
-//Procesa los parametros de la solicitud POST
-function processRequestData(request, callback) {
-    let data = [];
-    request.on('data', (chunk) => {
-        data.push(chunk);
-    }).on('end', () => {
-        data = JSON.parse(Buffer.concat(data).toString());
-        callback(data);
-    });
-}
-
-//Obtener extension de un nombre de archivo
-function getExtension(filename) {
-    var ext = path.extname(filename || '').split('.');
-    return ext[ext.length - 1];
-}
-
-//Obtiene todos los archivos de un directorio
-function getFileSystem(dir, files_) {
-    files_ = files_ || {
-        files: [],
-        folders: []
-    };
-    var files = fs.readdirSync(dir);
-    for (var i in files) {
-        var name = dir + '/' + files[i];
-        elem = fs.statSync(name);
-        if (elem.isDirectory()) {
-            files_.folders.push(name);
-            getFileSystem(name, files_);
-        } else {
-            var fileSize = elem.size;
-            fileSize /= 1048576;
-            files_.files.push({
-                name: name,
-                size: formatearFloat(fileSize)
-            });
-        }
-    }
-    return files_;
-}
-
-function getFileNameFromURL(fileURL) {
-    return fileURL.substr(fileURL.lastIndexOf('/') + 1)
-}
-
-function deleteFile(req, res, rutaArchivo) {
-    fs.unlink(rutaArchivo, (err) => {
-        if (err) {
-            log("FILE NOT DELETED: " + rutaArchivo);
-            return res.end("No se puede borrar el archivo");
-        };
-        log("FILE DELETED: " + rutaArchivo);
-        return res.end("OK");
-    });
-}
-
-function renameFile(rutaArchivo, ruta, newName,res) {
-    fs.rename(rutaArchivo, ruta + newName, (err) => {
-        if (err) {
-            log(err)
-            log("FILE NOT UPDATED: " + rutaArchivo);
-            return res.end("ERROR");
-        };
-        log("FILE UPDATED: " + rutaArchivo);
-        return res.end("OK");
-    });
-}
-
-function writeFile(req, res, rutaArchivo, nuevoContenido) {
-    fs.writeFile(rutaArchivo, nuevoContenido, (err) => {
-        if (err) {
-            log("FILE WRITTED: " + rutaArchivo);
-            return res.end("No se puede escribir el archivo");
-        };
-        log("FILE WRITTED: " + rutaArchivo);
-        return res.end("OK");
-    });
-}
-
-function notFound(req, res) {
-    log("NOT FOUND:  " + req.url);
-    fs.readFile('./inc/404-Not-Found.html', function (err, text) {
-        if (err) {
-            log(text);
-            log("ERROR:  " + err);
-            return res.end("ERROR:  " + req.url);
-        } else {
-            res.setHeader("Content-Type", "text/html");
-            return res.end(text);
-        }
-    });
-}
-
-function isBlog(hostName) {
-    if (hostName.includes('blog.')) {
-        log('Its the blog')
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function formatearFloat(n, d) {
-    // parsea el string n a float y lo limita a 2 decimales
-    return parseFloat(parseFloat(n).toFixed(d ? d : 2));
-}
-
-//Imprime objeto
-function log(o) {
-    console.log(o);
 }
